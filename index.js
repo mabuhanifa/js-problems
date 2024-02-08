1c0cf85f0c84b4d3ec4f1771b8164b68d3c9737b
mabuhanifa <moh.abuhanifa@gmail.com>
2024-02-08 18:03:14 +0600

    findTarget Problem

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const { log: print } = console;
+const { log } = console;
 
-print("Hello World");
+function findTarget(target, array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+log(findTarget(9, [2, 7, 11, 15]));
