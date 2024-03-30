87eb2395f96e55060d81cfae9e1b7aa31d673249
mabuhanifa <moh.abuhanifa@gmail.com>
2024-03-30 23:33:06 +0600

    test_prime

--- index.js
+++ index.js
@@ -57,3 +57,19 @@ const matches = (obj, source) =>
   Object.keys(source).every(
     (key) => obj.hasOwnProperty(key) && obj[key] === source[key]
   );
+
+function test_prime(n) {
+  if (n === 1) {
+    return false;
+  } else if (n === 2) {
+    return true;
+  } else {
+    for (var x = 2; x < n; x++) {
+      if (n % x === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+console.log(test_prime(37));
