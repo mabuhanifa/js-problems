47621b1f3b68ef7c0ae75c6dfd8db2eede53c66d
mabuhanifa <moh.abuhanifa@gmail.com>
2024-04-07 23:59:00 +0600

    removeArrayElement

--- index.js
+++ index.js
@@ -100,3 +100,25 @@ function truncateWithWordLimit(str, wordLimit) {
 }
 
 console.log(`Truncated string: ${truncateWithWordLimit(str, wordLimit)}`);
+
+// removeArrayElement("money") returns the array without the money object
+// removeArrayElement("id") returns the array without the id object
+// removeArrayElement("cStatus") returns the array without the cStatus object
+
+const array = [
+  { field: "id", operator: "eq" },
+  { field: "cStatus", operator: "eq" },
+  { field: "money", operator: "eq" },
+];
+
+const filterField = "money";
+
+function removeArrayElement(filterField) {
+  // write your solution here
+  let result = array.filter(function (obj) {
+    return obj.field !== filterField;
+  });
+  return result;
+}
+
+console.log(`filtered array: ${removeArrayElement(filterField)}`);
