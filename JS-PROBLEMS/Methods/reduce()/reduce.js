b2fb31bdc3ca85e547b545af42a2238296086319
mabuhanifa <moh.abuhanifa@gmail.com>
2024-04-26 23:55:20 +0600

    Counting Instances

--- JS-PROBLEMS/Methods/reduce()/reduce.js
+++ JS-PROBLEMS/Methods/reduce()/reduce.js
@@ -8,3 +8,10 @@ const flattenedArray = nestedArray.reduce(
   []
 );
 console.log(flattenedArray); // Output: [1, 2, 3, 4, 5, 6]
+
+const fruits = ["apple", "banana", "orange", "apple", "banana", "apple"];
+const fruitCount = fruits.reduce((acc, fruit) => {
+  acc[fruit] = (acc[fruit] || 0) + 1;
+  return acc;
+}, {});
+console.log(fruitCount);
