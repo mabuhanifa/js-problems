bd9c3c07b15f552745b639ce7dee566bb6c2014e
mabuhanifa <moh.abuhanifa@gmail.com>
2025-02-22 23:20:37 +0600

    function for finding average from numeric string

--- random.js
+++ random.js
@@ -16,4 +16,21 @@ function leapY(y) {
   return (y % 4 === 0 && y % 100 !== 0) || y % 400 === 0 ? true : false;
 }
 
-console.log(leapY(1996));
+// console.log(leapY(1996));
+
+const numString = "7qj1h2jn2j3pf54jh6k7m4";
+
+function avgFromString(str) {
+  let sum = 0;
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(str[i]) && str[i] !== " ") {
+      sum += Number(str[i]);
+      count++;
+    }
+  }
+
+  return count === 0 ? 0 : sum / count; // Prevent division by zero
+}
+console.log(avgFromString(numString));
